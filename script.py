3e357220d593af29236bba576ccfe9a930bd2e86
nzrsvt <nsavitsky89@gmail.com>
2024-04-12 13:36:01 +0300

    Provide the expected structure of the script functioning

--- script.py
+++ script.py
@@ -1,2 +1,24 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
+
+def get_spotify_playlist(url):
+    pass
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <urlAddress>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    playlist = get_spotify_playlist(url)
+
+    with open("playlist.txt", "w", encoding="utf-8") as file:
+        for artist, song in playlist.items():
+            file.write(f"{artist} - {song}\n")
+    print("Playlist saved to playlist.txt")
+
+    for idx, (artist, song) in enumerate(playlist.items(), start=1):
+        print(f"{idx}. {artist} - {song}")
